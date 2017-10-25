a5e4ad97d264ee002d2f0f21e42a69d819f674eb
Zhangxf <zxf@aek56.com>
2017-10-25 10:39:18 +0800

    汉化Git侧边栏菜单 添加常用Git功能

--- TortoiseGIT.py
+++ TortoiseGIT.py
@@ -4,154 +4,157 @@ import os
 import os.path
 import subprocess
 
-# Borrowed from http://bit.ly/1iDl6f6
-def current_dir(window):
-    """
-    Return the working directory in which the window's commands should run.
+class TortoiseGitCommand(sublime_plugin.WindowCommand):
+    def run(self, cmd, paths=None, isHung=False):
+        if paths is not None:
+            for index, path in enumerate(paths):
+              if "${PROJECT_PATH}" in path:
+                  project_data  = sublime.active_window().project_data()
+                  project_folder = project_data['folders'][0]['path']
+                  path = path.replace("${PROJECT_PATH}", project_folder);
+                  paths[index] = path
+        dir = self.get_path(paths)
 
-    In the common case when the user has one folder open, return that.
-    Otherwise, return one of the following (in order of preference):
-        1) One of the open folders, preferring a folder containing the active
-           file.
-        2) The directory containing the active file.
-        3) The user's home directory.
-    """
-    folders = window.folders()
-    if len(folders) == 1:
-        return folders[0]
-    else:
-        active_view = window.active_view()
-        active_file_name = active_view.file_name() if active_view else None
-        if not active_file_name:
-            return folders[0] if len(folders) else os.path.expanduser("~")
-        for folder in folders:
-            if active_file_name.startswith(folder):
-                return folder
-        return os.path.dirname(active_file_name)
+        if not dir:
+            return
 
+        settings = sublime.load_settings('TortoiseGIT.sublime-settings')
+        tortoiseproc_path = settings.get('tortoisegit_proc_path')
+        pathEncoding = settings.get('pathEncoding')
 
-class TortoiseGITBashCommand(sublime_plugin.WindowCommand):
-	def run(self, paths=None, isHung=False):
-		dir = current_dir(self.window)
+        if tortoiseproc_path is None or not os.path.isfile(tortoiseproc_path):
+            sublime.error_message('can\'t find TortoiseGitProc.exe,'
+                ' please config setting file' '\n   --sublime-TortoiseGit')
+            return
 
-		if not dir:
-			return
-			
-		settings = sublime.load_settings('TortoiseGIT.sublime-settings')
-		gitbash_path = settings.get('gitbash_path')
+        closeonend = settings.get('close_on_end')
+        if closeonend is None:
+            closeonend = 0
 
-		if not os.path.isfile(gitbash_path):
-			sublime.error_message(''.join(['can\'t find sh.exe (gitbash),',
-				' please config setting file', '\n   --sublime-TortoiseGIT']))
-			raise
+        cmd = '"' + tortoiseproc_path + '"' + ' /command:' + cmd + ' /closeonend:%s' % closeonend + ' /path:"%s"' % dir
+        # print (cmd)
 
-		command = 'cd %s & "%s" --login -i' % (dir, gitbash_path)
-		os.system(command)
+        proce = subprocess.Popen(cmd.encode(pathEncoding) if pathEncoding else cmd , stdout=subprocess.PIPE)
 
-	def getPath(self, paths):
-		path = None
-		if paths:
-			path = '*'.join(paths)
-		else:
-			view = sublime.active_window().active_view()
-			path = view.file_name() if view else None
+        # This is required, cause of ST must wait TortoiseGit update then revert
+        # the file. Otherwise the file reverting occur before Git update, if the
+        # file changed the file content in ST is older.
+        if isHung:
+            proce.communicate()
 
-		return path
+    def get_path(self, paths):
+        path = None
+        if paths:
+            path = '*'.join(paths)
+        else:
+            view = sublime.active_window().active_view()
+            path = view.file_name() if view else None
+
+        return path
+
+    def get_setting(self):
+        return sublime.load_settings('TortoiseGIT.sublime-settings')
 
 
-class TortoiseGITCommand(sublime_plugin.WindowCommand):
-	def run(self, cmd, paths=None, isHung=False):
-		dir = current_dir(self.window)
+class TortoiseGITBashCommand(TortoiseGitCommand):
+    def run(self, paths=None, isHung=False):
+        dir = self.get_path(paths)
+        # print (paths)
 
-		if not dir:
-			return
-			
-		settings = sublime.load_settings('TortoiseGIT.sublime-settings')
-		tortoisegit_path = settings.get('tortoisegit_path')
+        if not dir:
+            return
 
-		if not os.path.isfile(tortoisegit_path):
-			sublime.error_message(''.join(['can\'t find TortoiseGitProc.exe,',
-				' please config setting file', '\n   --sublime-TortoiseGIT']))
-			raise
+        settings = sublime.load_settings('TortoiseGIT.sublime-settings')
+        gitbash_path = settings.get('git_bash_path')
 
-		proce = subprocess.Popen('"' + tortoisegit_path + '"' +
-			' /command:' + cmd + ' /path:"%s"' % dir , stdout=subprocess.PIPE)
+        if gitbash_path is None or not os.path.isfile(gitbash_path):
+            sublime.error_message(''.join(['can\'t find sh.exe (gitbash),',
+                ' please config setting file', '\n   --sublime-TortoiseGIT']))
+            raise
 
-		# This is required, cause of ST must wait TortoiseGIT update then revert
-		# the file. Otherwise the file reverting occur before SVN update, if the
-		# file changed the file content in ST is older.
-		if isHung:
-			proce.communicate()
+        command = 'cd %s & "%s" --login -i' % (dir, gitbash_path)
+        os.system(command)
 
 
-class MutatingTortoiseGITCommand(TortoiseGITCommand):
-	def run(self, cmd, paths=None):
-		TortoiseGITCommand.run(self, cmd, paths, True)
-		
-		self.view = sublime.active_window().active_view()
-		row, col = self.view.rowcol(self.view.sel()[0].begin())
-		self.lastLine = str(row + 1);
-		sublime.set_timeout(self.revert, 100)
+class MutatingTortoiseGitCommand(TortoiseGitCommand):
+    def run(self, cmd, paths=None):
+        TortoiseGitCommand.run(self, cmd, paths, True)
 
-	def revert(self):
-		self.view.run_command('revert')
-		sublime.set_timeout(self.revertPoint, 600)
+        self.view = sublime.active_window().active_view()
+        row, col = self.view.rowcol(self.view.sel()[0].begin())
+        self.lastLine = str(row + 1);
+        sublime.set_timeout(self.revert, 100)
 
-	def revertPoint(self):
-		self.view.window().run_command('goto_line',{'line':self.lastLine})
+    def revert(self):
+        self.view.run_command('revert')
+        sublime.set_timeout(self.revertPoint, 600)
+
+    def revertPoint(self):
+        self.view.window().run_command('goto_line', {'line':self.lastLine})
 
 
-class GitCloneCommand(MutatingTortoiseGITCommand):
+class GitCloneCommand(MutatingTortoiseGitCommand):
+    def run(self, paths=None):
+        MutatingTortoiseGitCommand.run(self, 'clone', paths)
+
+class GitSwitchCommand(TortoiseGitCommand):
+    def run(self, paths=None):
+        TortoiseGitCommand.run(self, 'switch', paths)
+
+class GitStatusCommand(TortoiseGitCommand):
+    def run(self, paths=None):
+        TortoiseGitCommand.run(self, 'repostatus', paths)
+
+class GitCommitCommand(TortoiseGitCommand):
+    def run(self, paths=None):
+        TortoiseGitCommand.run(self, 'commit', paths)
+
+class GitPushCommand(TortoiseGitCommand):
 	def run(self, paths=None):
-		settings = sublime.load_settings('TortoiseGIT.sublime-settings')
-		closeonend = ('3' if True == settings.get('autoCloseUpdateDialog') else '0')
-		MutatingTortoiseGITCommand.run(self, 'clone /closeonend:' + closeonend, paths)
+		TortoiseGitCommand.run(self, 'push', paths)
 
+class GitFetchCommand(TortoiseGitCommand):
+    def run(self, paths=None):
+        TortoiseGitCommand.run(self, 'fetch', paths)
 
-class GitCommitCommand(TortoiseGITCommand):
+class GitPullCommand(TortoiseGitCommand):
 	def run(self, paths=None):
-		TortoiseGITCommand.run(self, 'commit /closeonend:3', paths)
-
-class GitCheckoutCommand(TortoiseGITCommand):
-	def run(self, paths=None):
-		TortoiseGITCommand.run(self, 'checkout /closeonend:3', paths)
-
-class GitPushCommand(TortoiseGITCommand):
-	def run(self, paths=None):
-		TortoiseGITCommand.run(self, 'push /closeonend:3', paths)
-
-class GitPullCommand(TortoiseGITCommand):
-	def run(self, paths=None):
-		TortoiseGITCommand.run(self, 'pull /closeonend:3', paths)
+		TortoiseGitCommand.run(self, 'pull', paths)
 
 class GitBashCommand(TortoiseGITBashCommand):
-	def run(self, paths=None):
-		TortoiseGITBashCommand.run(self, paths)
+    def run(self, paths=None):
+        TortoiseGITBashCommand.run(self, paths)
 
-"""
-class GitRevertCommand(MutatingTortoiseGITCommand):
-	def run(self, paths=None):
-		MutatingTortoiseGITCommand.run(self, 'revert', paths)
-
-
-class GitLogCommand(TortoiseGITCommand):
-	def run(self, paths=None):
-		TortoiseGITCommand.run(self, 'log', paths)
+    # def is_visible(self, paths=None):
+    #     print (paths)
+    #     if paths is None:
+    #         return True
+    #     else:
+    #         return os.path.isdir(paths)
 
 
-class GitDiffCommand(TortoiseGITCommand):
-	def run(self, paths=None):
-		TortoiseGITCommand.run(self, 'diff', paths)
+class GitRevertCommand(MutatingTortoiseGitCommand):
+    def run(self, paths=None):
+        MutatingTortoiseGitCommand.run(self, 'revert', paths)
 
 
-class GitBlameCommand(TortoiseGITCommand):
-	def run(self, paths=None):
-		view = sublime.active_window().active_view()
-		row = view.rowcol(view.sel()[0].begin())[0] + 1
+class GitLogCommand(TortoiseGitCommand):
+    def run(self, paths=None):
+        TortoiseGitCommand.run(self, 'log', paths)
 
-		TortoiseGITCommand.run(self, 'blame /line:' + str(row), paths)
 
-	def is_visible(self, paths=None):
-		file = self.getPath(paths)
-		return os.path.isfile(file) if file else False
-"""
+class GitDiffCommand(TortoiseGitCommand):
+    def run(self, paths=None):
+        TortoiseGitCommand.run(self, 'diff', paths)
+
+
+class GitBlameCommand(TortoiseGitCommand):
+    def run(self, paths=None):
+        view = sublime.active_window().active_view()
+        row = view.rowcol(view.sel()[0].begin())[0] + 1
+
+        TortoiseGitCommand.run(self, 'blame /line:' + str(row), paths)
+
+    def is_visible(self, paths=None):
+        file = self.get_path(paths)
+        return os.path.isfile(file) if file else False
